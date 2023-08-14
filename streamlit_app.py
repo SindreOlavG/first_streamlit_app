fadb32e3d30473052741ddf93f739a9aa7e88ab1
SindreOlavG <141735831+SindreOlavG@users.noreply.github.com>
2023-08-14 11:24:07 +0200

    Update streamlit_app.py
    Adding code to stop streamlit running while we troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+# code so streamlit doesn't run anything past here while troubleshooting
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
